be3f65b99d9a7d804e5488a07c7111c364f25bdd
emarkovic <enamarkovic27@gmail.com>
2017-09-10 12:04:08 -0700

    moved recipe store and file into recipe folder, made store interface for recipe

---
+++ recipesvr/models/recipes/store.go
@@ -0,0 +1,24 @@
+package recipes
+
+// Store represents an abstract store for model.Recipe objects
+// This interface is used by the HTTP handlers to insert new recipes,
+// get previously inserted recipes and modify and delete inserted recipes.
+// This interface can be implemented for any persistant database
+type Store interface {
+	// GetRecipes gets all recipes a given user is allowed to see
+	// TODO: pass userId
+	GetRecipes() ([]*Recipe, error)
+
+	// InsertRecipe inserts a new recipe into the db
+	// TODO: pass userId
+	InsertRecipe(newRecipe *NewRecipe) (*Recipe, error)
+
+	// FavoriteRecipe favorites a recipe
+	FavoriteRecipe(updates *RecipeUpdates, id RecipeID) (*Recipe, error)
+
+	// ScheduleRecipe saves which day the user applied to the recipe
+	ScheduleRecipe(updates *RecipeUpdates, id RecipeID, day Day) (*Recipe, error)
+
+	// DeleteRecipe removes a recipe the user has previously saved
+	DeleteRecipe(id RecipeID) error
+}

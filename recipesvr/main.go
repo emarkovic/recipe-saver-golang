deecb6ea9be86bdb52360fb72ae94bbe59087d80
emarkovic <enamarkovic27@gmail.com>
2017-09-11 12:43:34 -0700

    setting up mngo connection

--- recipesvr/main.go
+++ recipesvr/main.go
@@ -5,6 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/info344-in-class/tasksvr/handlers"
+	"github.com/recipe-saver-golang/recipesvr/models/recipes"
+	"github.com/recipe-saver-golang/recipesvr/models/users"
+
+	mgo "gopkg.in/mgo.v2"
 )
 
 const (
@@ -56,10 +62,23 @@ func main() {
 		host = "localhost"
 	}
 
-	// ctx := &handlers.Context{
-	// 	UserStore: users.NewMongoStore(mgoSID),
-	// 	RecipeStore: recipes.NewMongoStore(mgoSID),
-	// }
+	// set up mongo connection
+	dbAddr := os.Getenv("DBADDR")
+	var mgoSID *mgo.Session
+	var err error
+	if len(dbAddr) > 0 {
+		mgoSID, err = mgo.Dial(dbAddr)
+	} else {
+		mgoSID, err = mgo.Dial("127.0.0.1:27017")
+	}
+	if err != nil {
+		log.Fatalf("error dialing Mongo: %v", err)
+	}
+
+	ctx := &handlers.Context{
+		UserStore:   users.NewMongoStore(mgoSID),
+		RecipeStore: recipes.NewMongoStore(mgoSID),
+	}
 
 	http.HandleFunc("/", defaultMsg)
 	http.HandleFunc("/v1/recipes", recipesHander)

ea670a3c989411a72dc559bb6d55c9226b4bdf90
emarkovic <enamarkovic27@gmail.com>
2017-09-17 11:53:08 -0700

    added methods to user store and implemented them in user mongostore

--- recipesvr/models/users/mongostore.go
+++ recipesvr/models/users/mongostore.go
@@ -1,6 +1,7 @@
 package users
 
 import mgo "gopkg.in/mgo.v2"
+import "gopkg.in/mgo.v2/bson"
 
 // MongoStore is a struct representing mongo
 type MongoStore struct {
@@ -18,8 +19,47 @@ func NewMongoStore(sessionID *mgo.Session) *MongoStore {
 	}
 }
 
-//Insert inserts a new NewUser into the store
-//and returns a User with a newly-assigned ID
+// Insert inserts a new NewUser into the store
+// and returns a User with a newly-assigned ID
 func (ms *MongoStore) Insert(newUser *NewUser) (*User, error) {
-	return nil, nil
+	user, err := newUser.ToUser()
+	if err != nil {
+		return nil, err
+	}
+
+	user.ID = UserID(bson.NewObjectId().Hex())
+	err = ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).Insert(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+// GetByEmail returns a user with the provided email address
+func (ms *MongoStore) GetByEmail(email string) (*User, error) {
+	user := &User{}
+
+	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).Find(bson.M{"email": email}).One(user)
+	if err == mgo.ErrNotFound {
+		return nil, ErrUserNotFound
+	} else if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+// GetByID returns a user with the provided user id
+func (ms *MongoStore) GetByID(userID UserID) (*User, error) {
+	user := &User{}
+
+	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).FindId(string(userID)).One(user)
+	if err == mgo.ErrNotFound {
+		return nil, ErrUserNotFound
+	} else if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
--- recipesvr/models/users/store.go
+++ recipesvr/models/users/store.go
@@ -2,18 +2,24 @@ package users
 
 import "errors"
 
-//ErrUserNotFound is returned when the requested user is not found in the store
+// ErrUserNotFound is returned when the requested user is not found in the store
 var ErrUserNotFound = errors.New("user not found")
 
-//ErrEmailAlreadyExists is returned when the email on insert already exists in the store
+// ErrEmailAlreadyExists is returned when the email on insert already exists in the store
 var ErrEmailAlreadyExists = errors.New("email is already taken")
 
-//Store represents an abstract store for model.User objects.
-//This interface is used by the HTTP handlers to insert new users,
-//get users, and update users. This interface can be implemented
-//for any persistent database you want (e.g., MongoDB, PostgreSQL, etc.)
+// Store represents an abstract store for model.User objects.
+// This interface is used by the HTTP handlers to insert new users,
+// get users, and update users. This interface can be implemented
+// for any persistent database you want (e.g., MongoDB, PostgreSQL, etc.)
 type Store interface {
-	//Insert inserts a new NewUser into the store
-	//and returns a User with a newly-assigned ID
+	// Insert inserts a new NewUser into the store
+	// and returns a User with a newly-assigned ID
 	Insert(newUser *NewUser) (*User, error)
+
+	// GetByEmail returns a user with the provided email address
+	GetByEmail(email string) (*User, error)
+
+	// GetByID returns a user with the provided user id
+	GetByID(userID UserID) (*User, error)
 }

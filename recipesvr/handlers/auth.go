e3301d486d17d27a86ceb3d66800a3ac41a71d77
emarkovic <enamarkovic27@gmail.com>
2017-11-12 11:14:10 -0800

    added signout stub to the sessions handler

--- recipesvr/handlers/auth.go
+++ recipesvr/handlers/auth.go
@@ -14,14 +14,18 @@ func (ctx *Context) UsersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("you are trying to create a new user"))
 }
 
-// SessionsHandlers handles sign ins for existing users
-// /v1/sessions -> post
+// SessionsHandlers handles sign ins/outs for existing users
+// /v1/sessions -> post -> signs in
+// /v1/sessions -> delete -> signs out
 func (ctx *Context) SessionsHandlers(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "invalid request method", http.StatusBadRequest)
-		return
+	w.Header().Add("Content-Type", "text/plain")
+	switch r.Method {
+	case "POST":
+		w.Write([]byte("you are trying to sign in"))
+		// create the session
+	case "DELETE":
+		w.Write([]byte("you are trying to sign out"))
+		// delete the session
 	}
 
-	w.Header().Add("Content-Type", "text/plain")
-	w.Write([]byte("you are trying to sign in"))
 }
